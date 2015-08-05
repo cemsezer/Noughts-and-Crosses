eda75ffa95fda94e136f05ef4aff8c5850dc73ab
cemsezer <icemsezer@gmail.com>
2015-08-05 16:19:56 +0100

    Added method to set the board when a player makes a move

--- NoughtsAndCrosses.Console/Program.cs
+++ NoughtsAndCrosses.Console/Program.cs
@@ -2,25 +2,34 @@
 
 namespace NoughtsAndCrosses.Console
 {
+    public class Game
+    {
+        private List<CellStatus> _board;
+        public enum CellStatus {Empty, Player1, Player2 }
+
+        public List<CellStatus> InitializeBoard()
+        {
+            _board = new List<CellStatus>();
+
+            for (var i = 0; i < 9; i++)
+            {
+                _board.Add(CellStatus.Empty);
+            }
+
+            return _board;
+        }
+
+        public List<CellStatus> MakeMove(CellStatus cellStatus, int cellIndex)
+        {
+            _board[cellIndex] = cellStatus;
+            return _board;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-        }
-    }
-
-    public class Game
-    {
-        public List<string> InitializeBoard()
-        {
-            var board = new List<string>();
-
-            for (var i = 0; i < 9; i++)
-            {
-                board.Add(string.Empty);
-            }
-
-            return board;
         }
     }
 }
--- NoughtsAndCrosses.Tests/NoughtsAndCrossesTests.cs
+++ NoughtsAndCrosses.Tests/NoughtsAndCrossesTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NoughtsAndCrosses.Console;
 
 namespace NoughtsAndCrosses.Tests
@@ -7,7 +8,7 @@ namespace NoughtsAndCrosses.Tests
     public class NoughtsAndCrossesTests
     {
         [TestMethod]
-        public void GivenGameInitialises_WhenGameStarts_ShouldHaveEmptyBoard()
+        public void GivenGameInitializes_WhenGameStarts_ShouldHaveEmptyBoard()
         {
             // Assign
             var game = new Game();
@@ -20,7 +21,31 @@ namespace NoughtsAndCrosses.Tests
             
             foreach (var cell in board)
             {
-                Assert.AreEqual(string.Empty, cell);
+                Assert.AreEqual(Game.CellStatus.Empty, cell);
+            }
+        }
+
+        [TestMethod]
+        public void GivenGameInitializes_WhenPlayer1MakesAMove_ThenBoardShouldShowThis()
+        {
+            // Assign
+            var game = new Game();
+            var board = game.InitializeBoard();
+            var randomCell = new Random().Next(0, 8);
+
+            // Act
+            board = game.MakeMove(Game.CellStatus.Player1, randomCell);
+
+            // Assert
+            for (var i = 0; i < board.Count; i++)
+            {
+                if (i == randomCell)
+                {
+                    Assert.AreEqual(Game.CellStatus.Player1, board[randomCell]);
+                    continue;
+                }
+
+                Assert.AreEqual(Game.CellStatus.Empty, board[i]);
             }
         }
     }
