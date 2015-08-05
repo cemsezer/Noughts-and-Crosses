5d07b516abb648e1bbfa23c2830e73704d6ba281
cemsezer <icemsezer@gmail.com>
2015-08-05 15:45:56 +0100

    Writing first unit test for NoughtsAndCrosses project

---
+++ NoughtsAndCrosses.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace NoughtsAndCrosses.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Game
+    {
+        public List<string> InitializeBoard()
+        {
+            var board = new List<string>();
+
+            for (var i = 0; i < 9; i++)
+            {
+                board.Add(string.Empty);
+            }
+
+            return board;
+        }
+    }
+}
---
+++ NoughtsAndCrosses.Tests/NoughtsAndCrossesTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NoughtsAndCrosses.Console;
+
+namespace NoughtsAndCrosses.Tests
+{
+    [TestClass]
+    public class NoughtsAndCrossesTests
+    {
+        [TestMethod]
+        public void GivenGameInitialises_WhenGameStarts_ShouldHaveEmptyBoard()
+        {
+            // Assign
+            var game = new Game();
+
+            // Act
+            var board = game.InitializeBoard();
+
+            //Assert
+            Assert.AreEqual(9, board.Count);
+            
+            foreach (var cell in board)
+            {
+                Assert.AreEqual(string.Empty, cell);
+            }
+        }
+    }
+}
